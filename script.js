06a39fa8a14daca14b91462b1802a67491ffbaea
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-14 00:03:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	var ans=str[0];
+	var cnt=0;
+	for(var i=1;i<str.length;i++){
+		if(ans==str[i]){
+			cnt++;
+		}else{
+			if(cnt==0){
+				return ans;
+			}else{
+				cnt=0;
+				ans=str[i];
+			}
+		}
+	}
+	
+	 return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
